109d7edcd2dd6c5c689ef34d84f66c523bfd4ddc
Rajesh <62700386+sjha707@users.noreply.github.com>
2023-01-08 21:58:16 +0530

    signup changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const database = require("./database/database");
 const userController = require("./controllers/userController");
 const newUserRegistration = require("./models/userSchema");
@@ -12,15 +14,28 @@ const port = process.env.PORT;
 // use of middle ware section
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use(cors());
-app.use("/api", userController);
+
+//
+app.post("/api", userController);
 
 // defaulr Routes Section here
 
 app.get("/", (req, res) => {
   // res.send("Index File Is Working Now ");
-  res.json({ success: true, message: "Wlecome To Backend Zone !" });
+  res.json({
+    success: true,
+    message: "Wlecome To Backend Zone Trying section !",
+  });
 });
+
+// User Creation Section
+
+// app.post("/signup", (req, res) => {
+//   console.log(req.body);
+//   res.send("There is Post API Called ...");
+// });
 
 app.listen(port, () => {
   console.log(`server is runnning on poort ${port}`);
