218af7e3c932b226b28091a9898aaa1e2e460ece
Rajesh <62700386+sjha707@users.noreply.github.com>
2023-01-08 22:44:11 +0530

    change in userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,153 +1,145 @@
-// init value section 
+// init value section
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// user schema 
+// user schema
 const userSchema = mongoose.Schema({
+  // inserted data into data base is
+  // {name , email , password }
 
-    // inserted data into data base is 
-    // {name , email , password }
-   
-    name:{
-        type : String,
-        required : [true , "Name Field Should Not be Blank"],
-        // min : 3,
+  name: {
+    type: String,
+    required: [true, "Name Field Should Not be Blank"],
+    // min : 3,
+  },
+  username: {
+    type: String,
+    default: "",
+  },
+  email: {
+    type: String,
+    required: [true, "Email Field Should Not be Blank"],
+    trim: true,
+    unique: [true, "This email is Already In Use "],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    min: 6,
+    required: [true, "Password Field Should Not be Blank"],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now(),
+  },
+  profilepicture: {
+    type: String,
+    default: "",
+  },
+  coverpicture: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    default: "",
+  },
+  byear: {
+    type: Number,
+    default: "",
+    trim: true,
+  },
+  bmonth: {
+    type: Number,
+    default: "",
+    trim: true,
+  },
+  bday: {
+    type: Number,
+    default: "",
+    trim: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  friends: {
+    type: Array,
+    default: [],
+  },
+  following: {
+    type: Array,
+    default: [],
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  request: {
+    type: Array,
+    default: [],
+  },
+  details: {
+    bio: {
+      type: String,
+      default: "",
     },
-    username :{
-        type :String,
-        default : false,
+    panname: {
+      type: String,
+      default: "",
     },
-    email :{
-        type: String,
-        required: [true, "Email Field Should Not be Blank"],
-        trim : true,
-        unique : [true, "This email is Already In Use "],
-        lowercase : true,
-
+    jobtitle: {
+      type: String,
+      default: "",
     },
-    password:{
-        type :String ,
-        min:6,
-        required:[true,"Password Field Should Not be Blank"],
+    workplace: {
+      type: String,
+      default: "",
     },
-    isActive :{
-        type : Boolean,
-        default : true,
+    highschool: {
+      type: String,
+      default: "",
     },
-    createdOn :{
-        type : Date,
-        default : Date.now()
+    college: {
+      type: String,
+      default: "",
     },
-    profilepicture :{
-        type : String,
-        default : "",
+    currentcity: {
+      type: String,
+      default: "",
     },
-    coverpicture:{
-        type : String,
-        default :"",
+    hometown: {
+      type: String,
+      default: "",
     },
-    gender:{
-        type :String,
-        default : false,
+    relationship: {
+      type: String,
+      default: "",
+      // enum :['Single','In Relation','Married','Divorced'],
     },
-    byear:{
-        type : Number,
-        default:false,
-        trim:true,
+    instagram: {
+      type: String,
+      default: "",
     },
-    bmonth:{
-        type : Number,
-        default: false,
-        trim:true,
+    facebook: {
+      type: String,
+      default: "",
     },
-    bday:{
-        type : Number,
-        default : false,
-        trim: true,
+    youtube: {
+      type: String,
+      default: "",
     },
-    verified :{
-        type : Boolean,
-        default:false,
-    },
-    friends:{
-        type : Array,
-        default :[]
-    },
-    following :{
-        type :Array,
-        default: []
-    },
-    followers:{
-        type :Array,
-        default : []
-    },
-    request :{
-        type :Array,
-        default :[]
-    },
-    details :{
-        bio:{
-            type : String,
-            default :false,
-        },
-        panname :{
-            type : String,
-            default : false,
-        },
-        jobtitle :{
-            type :String,
-            default:false,
-        },
-        workplace :{
-            type: String,
-            default : false,
-        },
-        highschool :{
-            type:String,
-            default : false,
-
-        },
-        college :{
-            type : String,
-            default : false,
-        },
-        currentcity:{
-            type :String,
-            default :false,
-        },
-        hometown:{
-            type :String,
-            default : false,
-        },
-        relationship:{
-            type : String,
-            default :false,
-            // enum :['Single','In Relation','Married','Divorced'],
-        },
-        instagram:{
-            type :String,
-            default:false,
-        },
-        facebook:{
-            type : String,
-            default :false,
-        },
-        youtube:{
-            type :String,
-            default:false,
-        }
-
-
-    }
-
-
-
+  },
 });
 
 //create Model
 
-mongoose.model('newusers', userSchema);
+mongoose.model("newusers", userSchema);
 
-// module export section 
+// module export section
 
-module.exports = mongoose.model('newusers');
+module.exports = mongoose.model("newusers");
